0f70fefbe34702f70a257ae8ece9c33c0df5ca66
John Dell <john@mutations.ltd>
2020-03-24 12:28:46 -0700

    before_filter -> before_action
    Signed-off-by: John Dell <john@mutations.ltd>

--- app/controllers/uploader/attachments_controller.rb
+++ app/controllers/uploader/attachments_controller.rb
@@ -1,16 +1,16 @@
 module Uploader
   class AttachmentsController < ActionController::Metal
     include AbstractController::Callbacks
-  
-    before_filter :find_klass
-    before_filter :find_asset, :only => [:destroy]
-    
+
+    before_action :find_klass
+    before_action :find_asset, :only => [:destroy]
+
     def create
       @asset = @klass.new(params[:asset])
       @asset.uploader_create(params, request)
       render_resourse(@asset, 201)
     end
-    
+
     def update
       params[:id].present? ? update_asset : sort_assets
     end
@@ -19,7 +19,7 @@ module Uploader
       @asset.uploader_destroy(params, request)
       render_resourse(@asset, 200)
     end
-    
+
     protected
 
       def update_asset
@@ -35,7 +35,7 @@ module Uploader
         end
         render_json(:files => [])
       end
-    
+
       def find_klass
         @klass = Uploader.constantize(params[:klass])
         raise ActionController::RoutingError.new("Class not found #{params[:klass]}") if @klass.nil?
@@ -43,9 +43,9 @@ module Uploader
 
       def find_asset
         @asset = @klass.where(:public_token => params[:id]).first
-        raise ActionController::RoutingError.new("Asset not found by guid #{params[:id]}") if @asset.nil? 
+        raise ActionController::RoutingError.new("Asset not found by guid #{params[:id]}") if @asset.nil?
       end
-      
+
       def render_resourse(record, status = 200)
         if record.errors.empty?
           render_json({:files => [record]}, status)
@@ -53,12 +53,12 @@ module Uploader
           render_json(record.errors, 422)
         end
       end
-      
+
       def render_json(hash_or_object, status = 200)
         self.status = status
         self.content_type = Uploader.content_type(env["HTTP_USER_AGENT"])
         self.response_body = hash_or_object.to_json(:root => false)
       end
-      
+
   end
 end
